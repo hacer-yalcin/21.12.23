0da6b68a73cfe7189e608863c457b13b686ef142
hacer-yalcin <haceryalcnn@gmail.com>
2023-12-24 23:06:10 +0300

    first commit

---
+++ test_case.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from selenium.webdriver.support.wait import WebDriverWait #ilgili driverı bekleten yapı
+from selenium.webdriver.support import expected_conditions as ec #beklenen koşullar
+import pytest
+
+class Test_DemoClass:
+    #prefix => test_ 
+    def setup_method(self): #her test başlangıcında çalışacak fonk
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.saucedemo.com")
+        self.driver.maximize_window() 
+
+    def teardown_method(self): # her testinin bitiminde çalışacak fonk
+        self.driver.quit()
+        
+    @pytest.mark.parametrize("username,password",[("standard_user","secret_sauce"),("performance_glitch_user","secret_sauce"),("visual_user","secret_sauce")])
+    def test_valid_login(self,username, password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        menuButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"react-burger-menu-btn")))
+        menuButton.click()
+        logoutLink = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='logout_sidebar_link']")))
+        logoutLink.click()
+        sleep(5)
+        acceptedUsername = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='login_credentials']/h4")))
+        assert acceptedUsername.text == "Accepted usernames are:"
+
+
+
